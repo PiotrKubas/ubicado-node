6c94d049b893a8d6db1e31bdecb2ce64521c8e1e
PiotrKubas <48236473+PiotrKubas@users.noreply.github.com>
2020-11-14 16:32:39 +0100

    db10

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const port = process.env.PORT || 3000
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:bxzPfHKYklnE1XQS@cluster0.nvx90.mongodb.net/Cluster0?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:bxzPfHKYklnE1XQS@cluster0.nvx90.mongodb.net/ubicado?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("ubicado").collection("users").get();
   // perform actions on the collection object
   client.close();
 });
@@ -15,7 +15,14 @@ client.connect(err => {
 const app = express()
 
 app.get('/', (req,res) => {
-    res.send('Connected to DB')
+
+        client.connect(err => {
+            const collection = client.db("ubicado").collection("users").get();
+            res.send(collection)
+            client.close();
+      });
+      
+    
     
 })
 
