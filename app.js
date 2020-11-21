3235eca632efcad0e2d57b94fc320e3fb5f24085
PiotrKubas <48236473+PiotrKubas@users.noreply.github.com>
2020-11-21 20:16:33 +0100

    db

--- app.js
+++ app.js
@@ -9,19 +9,26 @@ mongoClient.connect(uri, {}, (error,client) => {
     if(error){
         console.log('Failed')
     }
+    else console.log('Success')
 
     const db = client.db('ubicado')
   
 
-    app.get("/",  (req, res) => {
+    
         db.collection('users').insertOne({
-            name: 'Henio'
+            name: 'Henio2'
         }, (error,result) =>{
             if(error) console.log('Adding failed', error)
             console.log(result.ops)
         })
+    app.get('/', (req,res) => {
+        db.collection('users').find({}).toArray((error,result) => {
+            console.log(result)
+        })
+    })
+        
     
-    });
+    
 
 })
 
