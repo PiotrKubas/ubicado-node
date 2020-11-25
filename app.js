a07d0a47869c95c5897312ad380c28d4b7c89f41
PiotrKubas <48236473+PiotrKubas@users.noreply.github.com>
2020-11-25 20:49:49 +0100

    db

--- app.js
+++ app.js
@@ -4,18 +4,16 @@ const port = process.env.PORT || 3000
 const app = express()
 const authRoute = require('./routes/auth')
 
-app.use('/user', authRoute)
-
-
-
-
-
 const mongoClient = require('mongodb').MongoClient;
 
 mongoClient.connect(process.env.DB_CONNECTION, { useNewUrlParser: true}, (error,client) => {
-
-        
+          console.log('Connected to db')
 })
+
+
+app.use(express.json())
+
+app.use('/user', authRoute)
 
 app.listen(port)
 
