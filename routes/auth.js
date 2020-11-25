12e04f84143196b98cfbd333eba197e1b6ab131f
PiotrKubas <48236473+PiotrKubas@users.noreply.github.com>
2020-11-25 22:05:42 +0100

    db

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,15 @@ const router = require('express').Router();
 const User = require('../model/User');
 const Joi = require('@hapi/joi');
 
-const schema = {
+const schema = Joi.object({
     name: Joi.string().min(6).required(),
     email: Joi.string().min(6).required().email(),
     password: Joi.string().min(6).required(),
-}
+})
 
 router.post('/register', async (req, res) => {
-   // const {error} = Joi.validate(req.body, schema);
-  //  if(error) return res.status(400).send(error.details[0].message);
+    const {error} = schema.validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
     const user = new User({
         name: req.body.name,
         email: req.body.email,
