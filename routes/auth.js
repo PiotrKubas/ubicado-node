66cd637d9249d9b7de9a397b391c8fc20bb6f53f
PiotrKubas <48236473+PiotrKubas@users.noreply.github.com>
2020-11-28 12:33:24 +0100

    db

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,21 @@ const router = require('express').Router();
 const User = require('../model/User');
 const Joi = require('@hapi/joi');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const schema = Joi.object({
+const registerSchema = Joi.object({
     name: Joi.string().min(6).required(),
     email: Joi.string().min(6).required().email(),
     password: Joi.string().min(6).required(),
 })
 
+const loginSchema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+})
+
 router.post('/register', async (req, res) => {
-    const {error} = schema.validate(req.body);
+    const {error} = registerSchema.validate(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
     const emailExist = await User.findOne({email: req.body.email})
@@ -32,7 +38,20 @@ router.post('/register', async (req, res) => {
     }
 })
 
-router.post('/login')
+router.post('/login', async (req,res) =>{
+    const {error} = loginSchema.validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findOne({email: req.body.email})
+    if(!user) return res.status(400).send('Account does not exist');
+
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+    if(!validPass) return res.status(400).send('Invalid password');
+
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('Bearer', token).send(token);
+    
+})
 
 
 module.exports = router;
