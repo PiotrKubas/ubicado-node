3a737e78d3fc287fbdc6fbd49f4e4d776faedc24
PiotrKubas <48236473+PiotrKubas@users.noreply.github.com>
2020-11-28 17:44:09 +0100

    db

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const verify = require('./verifyToken')
+const Profile = require('../model/Profile');
 
-router.get('/', verify, (req,res)=> {
-    res.send({id: req.user._id})
+router.get('/', verify, async (req,res)=> {
+    const user = req.user
+    const userProfile = await Profile.findOne({userId: user._id})
+    res.status(200).send(userProfile)
 })
 
 module.exports = router;
