4493500c76611e2fa06a82818740fd11ca12e5b4
PiotrKubas <48236473+PiotrKubas@users.noreply.github.com>
2021-01-18 21:09:52 +0100

    db

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,7 @@ const registerSchema = Joi.object({
     name: Joi.string().min(6).required(),
     email: Joi.string().min(6).required().email(),
     password: Joi.string().min(6).required(),
+    access: Joi.string().min(5)
 })
 
 const loginSchema = Joi.object({
