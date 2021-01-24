d334350d6be38b3277d61f157842e76c1ec280ee
PiotrKubas <48236473+PiotrKubas@users.noreply.github.com>
2021-01-24 14:32:29 +0100

    db

--- routes/meeting.js
+++ routes/meeting.js
@@ -37,9 +37,7 @@ router.get('/friends-meetings', verify, async (req,res)=> {
     const user = req.user
     const userProfile = await Profile.findOne({userId: user._id})
     if(!userProfile) return res.status(400).send('User not found')
-    const meetings = userProfile.friends.map((friend) => {
-        await Meeting.findOne({creatorId: friend._id})
-    })
+    const meetings =  await Meeting.findOne({creatorId: userProfile.friends[0]._id})
     res.status(200).send(meetings)
 })
 
