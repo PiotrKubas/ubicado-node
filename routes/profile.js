fef348746094c6ee67da1f0b31908adbd09d9dce
PiotrKubas <48236473+PiotrKubas@users.noreply.github.com>
2021-02-17 19:16:21 +0100

    db

--- routes/profile.js
+++ routes/profile.js
@@ -2,36 +2,37 @@ const router = require('express').Router();
 const verify = require('./verifyToken')
 const Profile = require('../model/Profile');
 
-router.get('/', verify, async (req,res)=> {
+router.get('/', verify, async (req, res) => {
     const user = req.user
-    const userProfile = await Profile.findOne({userId: user._id})
+    const userProfile = await Profile.findOne({ userId: user._id })
     res.status(200).send(userProfile)
 })
 
-router.put('/position', verify, async (req,res)=> {
+router.put('/position', verify, async (req, res) => {
     const user = req.user
-    const userProfile = await Profile.findOne({userId: user._id})
-    userProfile.position = req.body
+    const userProfile = await Profile.findOne({ userId: user._id })
+    const date = new Date()
+    userProfile.position = { ...req.body, date }
     await userProfile.save();
     res.status(200).send(userProfile);
 })
 
-router.put('/friends', verify, async (req,res)=> {
+router.put('/friends', verify, async (req, res) => {
     const user = req.user
-    const userProfile = await Profile.findOne({userId: user._id})
-    if(userProfile.friends.find((friend) => friend === req.body.name)) return res.status(400).send('It is already your friend');
-    const friendProfile = await Profile.findOne({name: req.body.name})
-    if(!friendProfile) return res.status(401).send('User not found');
+    const userProfile = await Profile.findOne({ userId: user._id })
+    if (userProfile.friends.find((friend) => friend === req.body.name)) return res.status(400).send('It is already your friend');
+    const friendProfile = await Profile.findOne({ name: req.body.name })
+    if (!friendProfile) return res.status(401).send('User not found');
     userProfile.friends.push(friendProfile.name);
     await userProfile.save();
     res.status(200).send(userProfile);
 })
 
-router.delete('/friends', verify, async (req,res)=> {
+router.delete('/friends', verify, async (req, res) => {
     const user = req.user
-    const userProfile = await Profile.findOne({userId: user._id})
+    const userProfile = await Profile.findOne({ userId: user._id })
     const friendToRemove = userProfile.friends.find(friend => friend === req.body.name)
-    if(!friendToRemove) return res.status(400).send('User not found');
+    if (!friendToRemove) return res.status(400).send('User not found');
     userProfile.friends = userProfile.friends.filter(friend => friend !== friendToRemove)
     await userProfile.save();
     res.status(200).send(userProfile);
