15e56058ccbd2d94155962cf08a242d357469939
PiotrKubas <48236473+PiotrKubas@users.noreply.github.com>
2021-06-23 18:39:32 +0200

    db

--- routes/profile.js
+++ routes/profile.js
@@ -5,12 +5,7 @@ const Profile = require('../model/Profile');
 router.get('/', verify, async (req, res) => {
     const user = req.user
     const userProfile = await Profile.findOne({ userId: user._id })
-    let foundFriend = null
-    const friends = await Promise.all(userProfile.friends.map(async friend => {
-        foundFriend = await Profile.findOne({ name: friend.name })
-        return { name: foundFriend.name, email: foundFriend.email, fullName: foundFriend.fullName, description: foundFriend.description, address: foundFriend.address } 
-    }))
-    res.status(200).send({...userProfile, friends})
+    res.status(200).send(userProfile)
 })
 
 router.put('/position', verify, async (req, res) => {
@@ -29,11 +24,7 @@ router.put('/update', verify, async (req, res) => {
     userProfile.address = req.body.address;
     userProfile.fullName = req.body.fullName;
     await userProfile.save();
-    let foundFriend = null
-    const friends = await Promise.all(userProfile.friends.map(async friend => {
-        foundFriend = await Profile.findOne({ name: friend.name })
-        return { name: foundFriend.name, email: foundFriend.email, fullName: foundFriend.fullName, description: foundFriend.description, address: foundFriend.address }     }))
-    res.status(200).send({...userProfile, friends});
+    res.status(200).send(userProfile);
 })
 
 router.put('/friends', verify, async (req, res) => {
@@ -43,15 +34,10 @@ router.put('/friends', verify, async (req, res) => {
     const friendProfile = await Profile.findOne({ name: req.body.name })
     if (!friendProfile) return res.status(401).send('User not found');
     userProfile.friends.push(
-        { name: friendProfile.name}
+        { name: friendProfile.name, email: friendProfile.email, fullName: friendProfile.fullName, description: friendProfile.description, address: friendProfile.address }
     );
     await userProfile.save();
-    let foundFriend = null
-    const friends = await Promise.all(userProfile.friends.map(async friend => {
-        foundFriend = await Profile.findOne({ name: friend.name })
-        return { name: foundFriend.name, email: foundFriend.email, fullName: foundFriend.fullName, description: foundFriend.description, address: foundFriend.address } 
-    }))
-    res.status(200).send({...userProfile, friends});
+    res.status(200).send(userProfile);
 })
 
 router.delete('/friends', verify, async (req, res) => {
@@ -61,12 +47,18 @@ router.delete('/friends', verify, async (req, res) => {
     if (!friendToRemove) return res.status(400).send('User not found');
     userProfile.friends = userProfile.friends.filter(friend => friend.name !== friendToRemove.name)
     await userProfile.save();
-    let foundFriend = null
+    res.status(200).send(userProfile);
+})
+
+router.get('/friends-info', verify, async (req, res) => {
+    const user = req.user
+    const userProfile = await Profile.findOne({ userId: user._id })
+    if (!userProfile) return res.status(400).send('User not found');
     const friends = await Promise.all(userProfile.friends.map(async friend => {
-        foundFriend = await Profile.findOne({ name: friend.name })
-        return { name: foundFriend.name, email: foundFriend.email, fullName: foundFriend.fullName, description: foundFriend.description, address: foundFriend.address } 
+        friendProfile = await Profile.findOne({ name: friend.name })
+        return  { name: friendProfile.name, email: friendProfile.email, fullName: friendProfile.fullName, description: friendProfile.description, address: friendProfile.address }
     }))
-    res.status(200).send({...userProfile, friends});
+    res.status(200).send(friends);
 })
 
 router.get('/friends-positions', verify, async (req, res) => {
